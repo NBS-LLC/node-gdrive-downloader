fefcf3e906f987f3a696472c5b589fdb382d2512
NBS-LLC <nick@networkbasedsolutions.com>
2022-04-04 22:21:44 -0700

    Cleaning up the sample app code.

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import { OAuth2Client } from 'google-auth-library';
+import { Credentials as AccessToken, OAuth2Client } from 'google-auth-library';
 import { google } from 'googleapis';
 import reader from 'readline-sync';
 
@@ -10,8 +10,10 @@ const HOME_DIR = require('os').homedir();
 const CONFIG_DIR = `${HOME_DIR}/.node-gdrive-downloader`;
 const TOKEN_PATH = `${CONFIG_DIR}/token.json`;
 
-const credentials = fs.readFileSync(`${CONFIG_DIR}/credentials.json`, { encoding: 'utf-8' });
-authorize(JSON.parse(credentials), listFiles);
+(async () => {
+    const credentials = fs.readFileSync(`${CONFIG_DIR}/credentials.json`, { encoding: 'utf-8' });
+    await authorize(JSON.parse(credentials), listFiles);
+})();
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -19,26 +21,27 @@ authorize(JSON.parse(credentials), listFiles);
  * @param credentials The authorization client credentials.
  * @param callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback) {
+async function authorize(credentials, callback) {
     const { client_secret, client_id, redirect_uris } = credentials.installed;
     const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
+    let token: AccessToken;
     try {
-        const token = fs.readFileSync(TOKEN_PATH, { encoding: 'utf-8' });
-        oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client);
+        token = JSON.parse(fs.readFileSync(TOKEN_PATH, { encoding: 'utf-8' }));
     } catch (error) {
-        getAccessToken(oAuth2Client, callback)
+        token = await getAccessToken(oAuth2Client);
     }
+
+    oAuth2Client.setCredentials(token);
+    callback(oAuth2Client);
 }
 
 /**
  * Get and store new token after prompting for user authorization, and then
  * execute the given callback with the authorized OAuth2 client.
  * @param oAuth2Client The OAuth2 client to get token for.
- * @param callback The callback for the authorized client.
  */
-function getAccessToken(oAuth2Client: OAuth2Client, callback) {
+async function getAccessToken(oAuth2Client: OAuth2Client): Promise<AccessToken> {
     const authUrl = oAuth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: SCOPES,
@@ -47,19 +50,20 @@ function getAccessToken(oAuth2Client: OAuth2Client, callback) {
     console.log('Authorize this app by visiting this url:', authUrl);
     const code = reader.question('Enter the code from that page here: ');
 
-    oAuth2Client.getToken(code, (err, token) => {
-        if (err || !token) return console.error('Error retrieving access token', err);
-        oAuth2Client.setCredentials(token);
-
+    try {
+        const { tokens } = await oAuth2Client.getToken(code);
         try {
-            fs.writeFileSync(TOKEN_PATH, JSON.stringify(token), { encoding: 'utf-8' });
+            fs.writeFileSync(TOKEN_PATH, JSON.stringify(tokens), { encoding: 'utf-8' });
             console.log('Token stored to', TOKEN_PATH);
         } catch (error) {
             console.error(error);
         }
 
-        callback(oAuth2Client);
-    });
+        return tokens;
+    } catch (error) {
+        console.error('Error retrieving the access token.');
+        throw error;
+    }
 }
 
 /**

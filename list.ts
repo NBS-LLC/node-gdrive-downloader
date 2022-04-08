73009bbcf51e8159c73ab456ef1f4c42a70c4a6e
NBS-LLC <nick@networkbasedsolutions.com>
2022-04-07 22:16:16 -0700

    Redesigned listFiles to fetch a list of files.
    * The function now returns a list of all media files.

--- list.ts
+++ list.ts
@@ -2,38 +2,49 @@ import { OAuth2Client } from 'google-auth-library';
 import { google } from 'googleapis';
 import { authorize } from './auth';
 
-const trialRun = false;
+const trialRun = true;
 
 (async () => {
     const auth = await authorize();
-    await listFiles(auth);
+    const mediaFiles = await fetchMediaFiles(auth);
+
+    console.log();
+    console.log(`Discovered ${mediaFiles.length} media files.`);
 })();
 
+interface MediaFile {
+    id: string,
+    name: string,
+    size: string
+}
+
 /**
- * List media files.
+ * Retrieve  list of media files.
  * @param auth An authorized OAuth2 client.
  */
-async function listFiles(auth: OAuth2Client) {
+async function fetchMediaFiles(auth: OAuth2Client): Promise<MediaFile[]> {
     const drive = google.drive({ version: 'v3', auth });
 
+    let fileList: MediaFile[] = [];
     let pageToken: string | undefined = undefined;
     do {
         const response = await drive.files.list({
             q: "'1hPyRfnkuCAGOVvmPdVFI9UYp_98-rHLN' in parents", // TODO: Search for the parent folder id based on name
+            orderBy: 'name_natural',
             pageSize: trialRun ? 10 : 1000,
-            fields: 'nextPageToken, files(id, name)',
+            fields: 'nextPageToken, files(id, name, size)',
             pageToken: pageToken
         });
 
         pageToken = trialRun ? undefined : response.data.nextPageToken;
-        const files = response.data.files;
+        const files: MediaFile[] = response.data.files;
 
-        if (files && files.length) {
+        if (files) {
+            fileList = fileList.concat(files);
             files.map((file) => {
-                console.log(`${file.name} (${file.id})`);
+                console.log(`${file.name} | ${file.id} | ${file.size}`);
             });
-        } else {
-            console.log('No files found.');
         }
     } while (pageToken);
+    return fileList;
 }
